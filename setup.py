297dadb8bb8847f805b8c1d1d25388e9cc54b943
Shin Oomura <lupin.oomura@example.com>
2024-11-20 01:11:00 +0900

    first commit

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mygoogleAuth',
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'flask',
         'flask-login',
         'oauthlib',
         'request',
+        'cryptography',
     ],
     url='https://github.com/lupin-oomura/mygoogleAuth.git',
     author='Shin Oomura',
